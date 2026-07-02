46712a866f8a4aa8a68b0ee08542e94f3bece8a2
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-02 14:59:40 +0530

    basic

--- Module_01/main.py
+++ Module_01/main.py
@@ -7,5 +7,16 @@ def hello():
     return "hello i am Nilesh Shah"
 
 @app.get("/users/{user_id}")
-def user(user_id : int): 
+def user(user_id : str): 
     return f"hello my id is = {user_id}"
+
+@app.get("/para")
+def parameter(name) : 
+    return {"Name" : name}
+
+
+@app.get("/items")
+def items(product : str = None , price :int = 0 ): 
+    return {"product" : product, 
+            "price" : price ,
+            }
