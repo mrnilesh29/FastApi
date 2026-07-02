070226913b8579cdf01bdf83e7bbd1697b0c8c39
Nilesh Shah <mr.nilesh29@gmail.com>
2026-07-02 14:22:52 +0530

    Dynamic route

--- Module_01/main.py
+++ Module_01/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello(): 
     return "hello i am Nilesh Shah"
+
+@app.get("/users/{user_id}")
+def user(user_id : int): 
+    return f"hello my id is = {user_id}"
